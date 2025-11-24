5e60f33df6d007d86e8138e99e51170082d39c41
Neha YoruNina <836193631@qq.com>
2025-11-25 02:51:52 +0800

    支持传送石碑路径点奖励

--- src/main/java/net/yorunina/ftbqaa/rewards/AARewardTypes.java
+++ src/main/java/net/yorunina/ftbqaa/rewards/AARewardTypes.java
@@ -7,5 +7,7 @@ import net.yorunina.ftbqaa.FTBQuestActuallyAdditions;
 
 public interface AARewardTypes {
     RewardType ASTAGE = RewardTypes.register(FTBQuestActuallyAdditions.id( "astages"), AStageReward::new, () -> Icons.CONTROLLER);
+    RewardType ACTIVE_WAY_STONE = RewardTypes.register(FTBQuestActuallyAdditions.id("active_way_stone"), ActiveWayStoneReward::new, () -> Icons.CONTROLLER);
+
     static void init() {}
 }
